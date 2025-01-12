639ffcca498dffdd0276e482154a0b6c6e5a7115
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2025-01-12 17:57:54 +0300

    -Добавил 2 задание -Добавил 3 задание

--- Task 2/Task 2.cpp
+++ Task 2/Task 2.cpp
@@ -1,5 +1,34 @@
 ﻿#include <iostream>
 
-int main() {
+int countWays(int target, int maxJumpLen = 3) {
+	//База: если n == 0, то есть 1 способ - остаться на текущей ступеньке
+	if (target == 0) {
+		return 1;
+	}
 
+	//База: если n < 0, способов нет
+	if (target < 0) {
+		return 0;
+	}
+
+	//Рекурсивно подсчитываю количество способов
+	int ways = 0;
+	for (int step = 1; step <= maxJumpLen; ++step)	{
+		ways += countWays(target - step, maxJumpLen);
+	}
+	
+	return ways;
+
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int target = 3;// Будет целевая ступенька
+
+	int maxJumpLen = 2;// Максимальная длина прыжка
+
+	std::cout << "Количество способов: " << countWays(target, maxJumpLen) << std::endl;
+
+	return 0;
 }
---
+++ Task 3/Task 3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+void evendigits(long long n, int& ans) {
+    // База: если число равно 0, завершаем рекурсию
+    if (n == 0) {
+        return;
+    }
+
+    //Проверяем, является ли последняя цифра чётной
+    if ((n % 10) % 2 == 0) {
+        ++ans;
+    }
+
+    //Рекурсивно обрабатываю оставшуюся часть числа
+    evendigits(n / 10, ans);
+}
+
+int main()  {
+    setlocale(LC_ALL, "Russian");
+
+    long long n = 9223372036854775806; //Пример числа
+    int ans = 0;
+
+    evendigits(n, ans);
+
+    std::cout << "Количество четных цифр: " << ans << std::endl;
+
+    return 0;
+}
